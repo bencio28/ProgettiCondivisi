6f8dd3670622efd8f46c3e3be2dd3a809335cf14
matteo <bencinimatteo@gmail.com>
2021-01-17 00:37:56 +0100

    App di prova in java che converte miglia in chilometri

---
+++ DistanceConverter/app/src/main/java/com/utilities/distanceconverter/MainActivity.java
@@ -0,0 +1,42 @@
+package com.utilities.distanceconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText milesToConvert = findViewById(R.id.miglia_da_convertire_editText);
+        TextView kmResult = findViewById(R.id.chilometri_corrispondenti_textView);
+        Button convertButton = findViewById(R.id.converti_button);
+
+        String  milesStringToConvert = milesToConvert.getText().toString();
+
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  milesStringToConvert = milesToConvert.getText().toString();
+                double miles = Double.parseDouble(milesStringToConvert);
+                double kilometers = miles * 1.6;
+                String finalResult = String.valueOf(kilometers);
+                kmResult.setText(finalResult);
+            }
+
+        });
+
+    }
+
+
+}
+
+
